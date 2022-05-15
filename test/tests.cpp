36706129c4ca972a074a02fc40f255a342ee5ed8
ArkharovDanil <99728665+ArkharovDanil@users.noreply.github.com>
2022-05-15 16:55:46 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,65 @@
-// Copyright 2021 GHA Test Team
+// Copyright 2022 GHA Test Team
 #include <gtest/gtest.h>
-#include "task1.h"
-#include "task2.h"
-#include "task3.h"
+#include "task.h"
+
+
+
+TEST(TEST1, 1) {
+    int cass_number = 5;
+    int max_num_of_clients = 120;
+    int clients_intensity = 15;
+    int serving_speed = 30;
+    int average_item_num = 10;
+    int max_line_len = 5;
+    Market FiveyorOchka(cass_number, max_num_of_clients, clients_intensity, serving_speed, average_item_num, max_line_len); 
+    FiveyorOchka.start();
+    ASSERT_EQ(result->FiveyorOchka.getAmountOfUnservedCustomers(), 115);
+}
+
+TEST(TEST2, 2) {
+    int cass_number = 5;
+    int max_num_of_clients = 120;
+    int clients_intensity = 15;
+    int serving_speed = 30;
+    int average_item_num = 10;
+    int max_line_len = 5;
+    Market FiveyorOchka(cass_number, max_num_of_clients, clients_intensity, serving_speed, average_item_num, max_line_len);
+    FiveyorOchka.start();
+    ASSERT_EQ(result->FiveyorOchka.getAmountOfServedCustomers(), 5);
+}
+
+TEST(TEST3, 3) {
+    int cass_number = 5;
+    int max_num_of_clients = 120;
+    int clients_intensity = 15;
+    int serving_speed = 30;
+    int average_item_num = 10;
+    int max_line_len = 5;
+    Market FiveyorOchka(cass_number, max_num_of_clients, clients_intensity, serving_speed, average_item_num, max_line_len);
+    FiveyorOchka.start();
+    ASSERT_EQ(result->FiveyorOchka.getAverageLineLength()(), 1;
+}
+
+TEST(TEST4, 4) {
+    int cass_number = 5;
+    int max_num_of_clients = 120;
+    int clients_intensity = 15;
+    int serving_speed = 30;
+    int average_item_num = 10;
+    int max_line_len = 5;
+    Market FiveyorOchka(cass_number, max_num_of_clients, clients_intensity, serving_speed, average_item_num, max_line_len);
+    FiveyorOchka.start();
+    ASSERT_EQ(FiveyorOchka.getAverageWaitingTimeForClient(), 300);
+}
+
+TEST(TEST5, 5) {
+    int cass_number = 5;
+    int max_num_of_clients = 120;
+    int clients_intensity = 15;
+    int serving_speed = 30;
+    int average_item_num = 10;
+    int max_line_len = 5;
+    Market FiveyorOchka(cass_number, max_num_of_clients, clients_intensity, serving_speed, average_item_num, max_line_len);
+    FiveyorOchka.start();
+    EXPECT_EQ(supermarket.getAverageWorkTime(), 1374);
+}
