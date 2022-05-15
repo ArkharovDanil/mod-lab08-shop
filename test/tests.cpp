20ea53f03cf65011b9bdc78e72f5f5a720cf0043
ArkharovDanil <99728665+ArkharovDanil@users.noreply.github.com>
2022-05-15 17:05:12 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -61,5 +61,5 @@ TEST(TEST5, 5) {
     int max_line_len = 5;
     Market FiveyorOchka(cass_number, max_num_of_clients, clients_intensity, serving_speed, average_item_num, max_line_len);
     FiveyorOchka.start();
-    EXPECT_EQ(supermarket.getAverageWorkTime(), 1374);
+    EXPECT_EQ(FiveyorOchka.getAverageWorkTime(), 1374);
 }
